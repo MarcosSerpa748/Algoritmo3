f46ca2bd58f59ecb04b84f97ea9686aafa70545e
marcos <msrserpa@gmail.com>
2026-07-30 22:04:50 -0300

    mmm

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] numeros = new int[]{20,1,7,2,12,39};
+        int maiorNumero = 0;
+        int segundoMaior = 1;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 0; i <= numeros.length-1;i++){
+
+            if (maiorNumero <=numeros[i]){
+                segundoMaior = maiorNumero;
+                maiorNumero = numeros[i];
+            }
         }
+        System.out.println("O  maior valor é:"+maiorNumero+" e o segundo é "+segundoMaior);
     }
+
 }
